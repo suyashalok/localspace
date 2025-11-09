69227fa67b35586333a5c3562c3075fa11944959
suyashalok <64395002+suyashalok@users.noreply.github.com>
2025-11-09 23:43:41 +0530

    API_connection

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import UserProfileListCreateView
+from .views import RegisterView
 
 urlpatterns = [
     path('', UserProfileListCreateView.as_view(), name='user-list'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
