5dde9a777bcf383bed13ddf3b43d5ffc778fa043
suyashalok <64395002+suyashalok@users.noreply.github.com>
2025-11-14 17:09:07 +0530

    listings_updates

--- listings/urls.py
+++ listings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListingListCreateView
+from .views import ListingListCreateView, ListingDetailView
 
 urlpatterns = [
     path('', ListingListCreateView.as_view(), name='listing-list'),
+    path('<int:pk>/', ListingDetailView.as_view(), name='listing-detail'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,20 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Listing
 from .serializers import ListingSerializer
 
 class ListingListCreateView(generics.ListCreateAPIView):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user.userprofile)
+
+
+class ListingDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_update(self, serializer):
+        serializer.save(created_by=self.request.user.userprofile)
